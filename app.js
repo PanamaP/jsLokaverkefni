f31708d19015efed63a96cbb16865042fb38e415
PanamaP <elfarsnaer@gmail.com>
2020-11-21 15:44:55 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,181 @@
+// uppskriftir nested object
+var uppskriftir = {
+    1 : {
+        author: "Kate Merker & Taylor Murray",
+        recipe: "Ground Turkey Sloppy Joes",
+        ingredients: ["2 tbsp. olive oil",
+        "1 medium yellow onion, chopped",
+        "1 red bell pepper, chopped",
+        "1 carrot, chopped",
+        "Kosher salt and freshly ground black pepper",
+        "2 cloves garlic, pressed",
+        "1 lb. lean ground turkey or chicken",
+        "1 tbsp. chili powder",
+        "1/4 tsp. ground cinnamon",
+        "1 (8-ounce) can tomato sauce",
+        "1/4 c. sweet relish",
+        "2 tsp. Worcestershire sauce",
+        "2 tbsp. red wine vinegar, divided",
+        "3 Persian cucumbers, thinly sliced",
+        "1/2 c. thinly sliced red onion",
+        "6 thick slices toast or split burger buns"],
+        instructions: `1. Heat oil in a large skillet over medium heat. 
+        Add yellow onion, bell pepper, and carrot. Season with salt and pepper. 
+        Cook, stirring occasionally, until tender, 6 to 8 minutes. 
+        Add garlic and cook, stirring occasionally, until fragrant, 30 seconds. 
+        Add turkey and cook, breaking it up with a spoon, until no longer pink, 
+        4 to 6 minutes. Add chili powder and cinnamon. 
+        Cook, stirring occasionally, 1 minute. Add tomato sauce and relish. 
+        Simmer until beginning to thicken, 2 to 4 minutes. 
+        Stir in Worcestershire and 1 tablespoon vinegar.
+        2. Toss together cucumbers, red onion, and remaining tablespoon vinegar in a bowl.
+        Season with salt. Let sit, tossing occasionally, 4 to 6 minutes. 
+        Spoon meat mixture on top of toast and top with pickles.`,
+        date: "Jan 6, 2020",
+        picture: "pictures/ground-turkey-sloppy-joes.jpg"
+        },
+    2 : {
+        author: "Ben",
+        recipe: "cod with caper brown butter",
+        ingredients: ["2 cod filets",
+        "2 tsp dried dill",
+        "2 tsp ground coriander",
+        "1 tsp ground turmeric",
+        "salt & pepper",
+        "4 tbsp butter",
+        "2 tbsp capers",
+        "2 cloves garlic, minced",
+        "Juice of 1 lemon"],
+        instructions: `1. Preheat oven to 175 degrees.
+        2. Add enough to a pan over medium heat to lightly coat the bottom.
+        3. Season fish with dill, coriander, turmeric, salt, and pepper. Add fish to pan and cook for 2 minutes; flip fish and cook for an additional 2 minutes.
+        4. Transfer fish from the pan to an oven proof dish and place it in the oven.
+        5. Over medium low heat add the butter, garlic, and capers. Sauté together for 5 minutes or so until butter begins to brown.
+        6. Remove the fish from the oven and add it back to the pan with the brown butter sauce. Baste the fish for a minute or so in the sauce and squeeze fresh lemon juice over the fish.
+        7. Now it's as simple as sitting back and enjoying this amazing recipe!`,
+        date: "April 8, 2017",
+        picture: "pictures/cod-with-caper-brown-butter.jpg"
+        },
+    3 : {
+        author: "Country Living Staff",
+        recipe: "Grilled Sausages with Toppings",
+        ingredients: `6 sweet or spicy Italian sausages
+        6 split rolls
+        Marinated Peppers and Onions, Grilled Horseradish Slaw, 
+        White Bean and Peppadew Salad, or Grilled Lemon Broccolini, for topping`,
+        instructions: "Heat grill to medium. Grill sausages, turning occasionally, until cooked through, 10 to 15 minutes. Serve in rolls with desired toppings.",
+        date: "Jun 26, 2019",
+        picture: "pictures/grilled-sausages.jpg"
+        },
+    4 : {
+        author: "Kate Merker & Taylor Murray",
+        recipe: "Crispy Chicken Thighs With Garlic and Rosemary",
+        ingredients: `1/3 c. chicken stock,
+        2 tbsp. fresh lemon juice,
+        1 tbsp. Dijon mustard,
+        1/4 tsp. red pepper flakes,
+        Kosher salt and freshly-ground black pepper,
+        8 small bone-in, skin-on chicken thighs (about 2 1/4 pounds total),
+        8 cloves garlic, smashes and skins removed`,
+        instructions: `1. Whisk together stock, lemon juice, mustard, rosemary, 
+        and pepper flakes in a bowl. Season with salt. 
+        2. Heat a 12-inch cast-iron skillet over medium heat until hot, 1 to 2 minutes. 
+        Season chicken with salt and pepper. Place, skin sides down, in the skillet. 
+        Place a second smaller skillet on top of chicken and weigh it down with soup cans. 
+        Cook until the skins are golden brown and crisp, 7 to 9 minutes. 
+        3. Remove cans and top skillet. Flip chicken and reduce heat to medium-low. 
+        Add garlic and cook until the chicken is cooked through, 2 to 3 minutes. 
+        Transfer chicken to a platter; reserve skillet. 
+        Add stock mixture to reserved skillet. 
+        Cook, scraping up any brown bits, until slightly thickened, 1 to 2 minutes. 
+        Spoon over chicken.`,
+        date: "Sep 6, 2019",
+        picture: "pictures/crispy-chicken-thighs-garlic-rosemary.png"
+        },
+    5 : {
+        author: "Kate Merker & Taylor Murray",
+        recipe: "Strip Steak With Lemon Mashed Potatoes",
+        ingredients: `1 1/2 lb. Yukon gold potatoes, peeled and cut into 2-inch chunks,
+        Kosher salt and freshly ground black pepper,
+        1 c. fresh or frozen peas,
+        2 tsp. lemon zest plus 2 tablespoons juice,
+        3 tbsp. olive oil, divided,
+        2 (3/4-inch-thick) strip steaks (about 1 1/2 pounds total),
+        2 small shallots, thinly sliced,
+        1/2 c. dry white wine,
+        1 1/2 tbsp. unsalted butter`,
+        instructions: `1. Preheat oven to 425°F. 
+        Place potatoes in a large pot; cover with cold salted water. 
+        Bring to a boil, reduce heat, and simmer until tender, 15 to 18 minutes, 
+        adding peas during the last 3 minutes of cooking. 
+        Drain and return vegetables to the pot. 
+        Add lemon zest and juice and 2 tablespoons oil; mash. 
+        Season with salt and pepper. 
+        2. Meanwhile, heat remaining tablespoon oil in a large skillet over medium-high heat. 
+        Season steak with salt and pepper. 
+        Cook to desired degree of doneness, 3 to 5 minutes per side for medium-rare. 
+        Transfer steak to cutting board and let rest 5 minutes before slicing; reserve skillet.
+        3. Reduce heat to medium. 
+        Add shallots to reserved skillet; cook, stirring occasionally, until tender, 
+        2 to 4 minutes. Add wine and 1/4 cup water. 
+        Cook, scraping up any browned bits with a wooden spoon, 
+        until liquid is reduced by half, 4 to 6 minutes; remove from heat. 
+        Add butter and swirl pan to melt; stir in juices from cutting board. 
+        Season with salt and pepper. Serve steak topped with sauce and potatoes alongside.`,
+        date: "Mar 15, 2019",
+        picture: "pictures/strip-steak-mashed-potatoes.jpg"
+        },
+};
+
+
+// Template
+
+var div = document.createElement("div");
+div.id = "container";
+document.body.appendChild(div);
+divID = document.getElementById("container");
+
+
+for(i in uppskriftir){
+    divItem = document.createElement("div");
+    divItem.classList.add("recipe");
+    divItem.id = "recipe_"+i;
+
+    
+    p = document.createElement("p");
+    author = document.createTextNode(`Author: ${uppskriftir[i].author}`);
+    divItem.appendChild(p).appendChild(author);
+
+    p = document.createElement("p");
+    recipe = document.createTextNode(`Recipe: ${uppskriftir[i].recipe}`)
+    divItem.appendChild(p).appendChild(recipe);
+
+    li = document.createElement("li");
+    ingredients = document.createTextNode(`Ingredients:`)
+    divItem.appendChild(li).appendChild(ingredients);
+
+    for(x in uppskriftir[i].ingredients){
+        item = document.createTextNode(uppskriftir[i].ingredients[x]);
+        li.appendChild(item);
+    }
+
+
+
+    uppskriftir[i].instructions
+    uppskriftir[i].date
+    uppskriftir[i].picture
+
+
+    
+    div.appendChild(divItem);
+};
+
+
+
+/*
+var x = document.createElement("IMG");
+x.setAttribute("src", uppskriftir[1].picture);
+x.setAttribute("width", "200");
+x.setAttribute("height", "200");
+document.body.appendChild(x);
+*/
